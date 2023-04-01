477e3600326b9ffccba9c1710a9a5357828fad7c
chandra kiran <chandrakiran@chandras-Mac.local>
2023-04-01 14:53:27 -0700

    upload to dropbox root

--- cloudplayer/Controllers/DownloadViewController.swift
+++ cloudplayer/Controllers/DownloadViewController.swift
@@ -8,22 +8,59 @@
 import UIKit
 
 class DownloadViewController: UIViewController {
-
+    var tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .insetGrouped)
+        table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+        return table
+    }()
+    
+    var audioFiles: [URL] = []
+    let fileManager = FileManager.default
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        fetchLocalAudioFiles()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func fetchLocalAudioFiles() {
+//        do {
+//            let documentsURL = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+//            let audioFiles = try fileManager.contentsOfDirectory(at: documentsURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles).filter { $0.pathExtension == "mp3" }
+//            self.audioFiles = audioFiles
+//            tableView.reloadData()
+//        } catch {
+//            print("Error fetching local audio files: \(error.localizedDescription)")
+//        }
+        // Start the search in the root directory
+let rootURL = try? fileManager.url(for: .downloadsDirectory, in: .localDomainMask, appropriateFor: nil, create: false) else {
+            print(audioFiles)
+               return audioFiles
+           }
+        
+        
+        
     }
-    */
+}
 
+extension DownloadViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return audioFiles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let audioFileURL = audioFiles[indexPath.row]
+        cell.textLabel?.text = audioFileURL.lastPathComponent
+        return cell
+    }
+}
+
+extension DownloadViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let audioFileURL = audioFiles[indexPath.row]
+//        let playerViewController = PlaybackPresenter.shared.startPlayBack(from: self, url: audioFileURL)
+        }
 }
--- cloudplayer/Controllers/UploadViewController.swift
+++ cloudplayer/Controllers/UploadViewController.swift
@@ -6,25 +6,88 @@
 //
 
 import UIKit
+import SwiftyDropbox
+import UniformTypeIdentifiers
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UIDocumentPickerDelegate {
 
+    var uploadButton: UIButton={
+       let button = UIButton()
+        button.setTitle("Upload", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.addTarget(self, action: #selector(uploadButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    var spinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-
-        // Do any additional setup after loading the view.
+        view.addSubview(uploadButton)
+        setupSpinner()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        uploadButton.frame = CGRect(x: (view.width - 60) / 2, y: (view.height - 30) / 2, width: 60, height: 30)
     }
-    */
-
+    
+    func setupSpinner() {
+        spinner = UIActivityIndicatorView(style: .large)
+        spinner.color = .blue
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.sizeToFit()
+        view.addSubview(spinner)
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc func uploadButtonTapped() {
+        let allowedtypes : [UTType] = [UTType.audio]
+        
+        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: allowedtypes)
+        documentPicker.delegate = self
+        present(documentPicker, animated: true, completion: nil)
+    }
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let url = urls.first else { return }
+        showSpinner()
+        let client = DropboxClientsManager.authorizedClient
+        do{
+        let uploadData = try Data(contentsOf: url)
+        
+        
+            client?.files.upload(path: "/audio/\(url.lastPathComponent)", mode: .add, input: uploadData).response { response, error in
+            self.hideSpinner()
+            if let response = response {
+                self.showalert(title: "Upload Success", message: "The file is uploaded")
+                print(response)
+            } else if let error = error {
+                print(error)
+                self.showalert(title: "Error", message: "There is an error in uploading, please try again later")
+            }
+        }
+        }
+        catch{
+            print("error occured")
+        }
+    }
+    func showalert(title:String, message:String){
+        let alert = UIAlertController(title: title,message:message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    func showSpinner() {
+        spinner.startAnimating()
+        uploadButton.isEnabled = false
+    }
+    
+    func hideSpinner() {
+        spinner.stopAnimating()
+        uploadButton.isEnabled = true
+    }
 }
